d2bfd1ee3c4e735dce3912ab47fd1149f0c7a1f0
ITihonya <itihonya_js@mail.ru>
2021-09-12 12:54:39 +0300

    upgrade component Link

--- src/components/GeneralComponents/Link/index.js
+++ src/components/GeneralComponents/Link/index.js
@@ -5,19 +5,8 @@ import {ViewBlock} from "../../../generalElements";
 const Link = (props) => {	
 	
 	return (
-		<ViewBlock 
-		justify="center"
-		alignI="center"
-		isPointer
-		borderR="4px"
-		color="#05000B"
-		fz="20px"
-		fw="600"
-		lh="32px"
-		inner="0px 35px 0px 15px"
-
-		>
-			<a href="google.com">{props.text_link}</a>
+		<ViewBlock {...props}>
+			<a href="google.com">{props.children}</a>
 		</ViewBlock>
 	);
 };
--- src/components/Сommunity/index.js
+++ src/components/Сommunity/index.js
@@ -60,9 +60,33 @@ const Community = () => {
 						</TextBlock>
 						<LinkBox>
 							<Galka/>
-							<Link text_link="High Analysis" />
+							<Link
+							justify="center"
+							alignI="center"
+							isPointer
+							borderR="4px"
+							color="#05000B"
+							fz="20px"
+							fw="600"
+							lh="32px"
+							inner="0px 35px 0px 15px" 							 
+							>
+								High Analysis
+							</Link>
 							<Galka/>
-							<Link text_link="Certified Institute" />
+							<Link
+							justify="center"
+							alignI="center"
+							isPointer
+							borderR="4px"
+							color="#05000B"
+							fz="20px"
+							fw="600"
+							lh="32px"
+							inner="0px 35px 0px 15px" 							
+							>
+								Certified Institute
+							</Link>
 						</LinkBox>
 						<Button						
 						backG="#FF5A05"					
